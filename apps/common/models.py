210c5eedfdbbc7b443ea3b9807007462a20cc4c9
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-14 13:20:54 +0500

    category model post

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,6 +1,13 @@
+import os
 from django.db import models
+from decimal import Decimal
+from django.dispatch import receiver
+from apps.common.slug import unique_slugify
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db.models.signals import post_migrate, post_delete
 
-# Create your models here.
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,3 +15,27 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    image = models.ImageField(upload_to='category/', null=True, blank=True, verbose_name=_("Image"))
+    icon = models.ImageField(upload_to='icon/', null=True, blank=True, max_length=55, verbose_name=_("Icon"))
+    order = models.PositiveIntegerField(default=0, verbose_name=_("Order"))
+    top = models.BooleanField(default=True, verbose_name=_("Top category"))
+    slug = models.SlugField(unique=True, verbose_name=_("Slug"))
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children',
+                               verbose_name=_("Parent"))
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+        ordering = ('oder',)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            unique_slugify(self, self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.parent.title} || {self.title}" if self.parent else self.title
