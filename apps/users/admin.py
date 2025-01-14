79f0c1b43886ef662614c2e97d3b035047b269d9
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-14 12:42:38 +0500

    user admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,23 @@
+from .models import User
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'phone', 'is_active', 'code')
+    search_fields = ('phone', 'first_name')
+    exclude = ('password',)
+    list_display_links = 'id', 'first_name', 'phone'
+
+    def code(self, obj):
+        return obj.verify_code.code
+
+    def is_active_(self, obj):
+        return obj.auth_status == 'code_verified'
+
+    code.short_description = _("Code")
+    is_active_.short_description = _("Is Active")
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('verify_code')
+
