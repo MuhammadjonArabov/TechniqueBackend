5ea474eb1df89347b39dec9c6a3aaf6084a2d076
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-14 14:14:36 +0500

    category post model

--- apps/common/models.py
+++ apps/common/models.py
@@ -39,3 +39,31 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.parent.title} || {self.title}" if self.parent else self.title
+
+
+class Product(BaseModel):
+    title = models.CharField(max_length=255, db_index=True, verbose_name=_("Title"))
+    price = models.DecimalField(max_digits=100, max_length=2, verbose_name=_("Price"))
+    price_uzs = models.DecimalField(max_digits=100, max_length=2, null=True, verbose_name=_("Price in UZS"))
+    discount = models.PositiveIntegerField(default=0, verbose_name=_("Discount"))
+    description = models.TextField(null=True, blank=True, verbose_name=_("Description"))
+    view_count = models.PositiveIntegerField(default=0, verbose_name=_("View Count"))
+    video_url = models.URLField(default='image.jfif', null=True, blank=True, verbose_name=_("Video Url"))
+    body = RichTextUploadingField(default='good', verbose_name=_("Body"))
+    on_sale = models.BooleanField(default=True, verbose_name=_("On Sale"))
+    is_many = models.BooleanField(default=True, verbose_name=_("Can you sell more?"))
+    slug = models.SlugField(unique=True, verbose_name=_("Slug"))
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="products",
+                                 limit_choices_to={'parent_isnull': False}, verbose_name=_("Category"))
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+
+    @property
+    def category_name(self):
+        return self.category.title
+
+    @property
+    def first_image(self):
+        return self.galleries.first().image
